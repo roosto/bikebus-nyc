93a895088e9e837720661439fed2dd958635645a
Glitch (bikebus) <none>
2022-08-30 20:25:38 +0000

    🐩🎤 Checkpoint ./src/pages/index.hbs:47893449/451 ./src/pages/test.hbs:47893449/6148 ./server.js:47893449/431

--- server.js
+++ server.js
@@ -46,6 +46,19 @@ fastify.get("/", async function (request, reply) {
   return reply.view("/src/pages/index.hbs", params);
 });
 
+fastify.get("/test", async function (request, reply) {
+  await storage.init();
+
+  // params is an object we'll pass to our handlebars template
+  let params = { 
+    latitude: await storage.getItem('latitude'),
+    longitude: await storage.getItem('longitude')
+  };
+
+  // The Handlebars code will be able to access the parameter values and build them into the page
+  return reply.view("/src/pages/test.hbs", params);
+});
+
 fastify.get("/beacon/"+process.env.beacon_hash, function (request, reply) {
   return reply.view("/src/pages/beacon.hbs");
 });
