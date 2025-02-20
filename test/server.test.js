22d1cc23c07a80cc3b341ee026c788ec18464204
Dustin Goldman <dustin@goldmans.net>
2025-02-19 22:26:42 -0500

    CheckPoint: busted with chai

--- test/server.test.js
+++ test/server.test.js
@@ -1,8 +1,27 @@
 const chai = require('chai');
 const assert = chai.assert;
+const chaiHttp = require('chai-http');
+const server = require('../server');
 
-describe('Arithmetic operations', () => {
-  it('should return the sum of two numbers', () => {
-    assert.equal(2 + 2, 4);
+// chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe('Loading the home page', () => {
+  it('Should load successfully, and display Manhattan Country School Map', (done) => {
+    server.inject({
+      method: 'GET',
+      url: '/'
+    }, (err, response) => {
+      expect(res).to.have.status(200);
+      console.log({err, response});
+    });
+    // chai.request(server)
+    //   .get('/')
+    //   .end((err, res) => {
+        
+    //     // expect(res.body.result).to.equal(5);
+    //     done();
+    //   });
   });
 });
