aeb02df344223a194d095445d99111c480ed33a8
Dustin Goldman <dustin@goldmans.net>
2025-02-19 23:03:20 -0500

    add more test cases

--- test/server.test.js
+++ test/server.test.js
@@ -11,3 +11,21 @@ test('requests the "/" route', async t => {
   t.equal(response.statusCode, 200, 'returns a status code of 200')
   t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
 })
+
+test('requests the "/manhattan-country-school/" route', async t => {
+  const response = await server.inject({
+    method: 'GET',
+    url: '/manhattan-country-school/'
+  })
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+  t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
+})
+
+test('requests the "/manhattan-country-school" route', async t => {
+  const response = await server.inject({
+    method: 'GET',
+    url: '/manhattan-country-school'
+  })
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+  t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
+})
