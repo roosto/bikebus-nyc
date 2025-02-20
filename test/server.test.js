9318675e2e5a929ad0b2fdf0090a6039185bc033
Dustin Goldman <dustin@goldmans.net>
2025-02-19 23:17:45 -0500

    refactor to be DRY
    setup and array of inputs and expected values, and have the main test logic loop through that

--- test/server.test.js
+++ test/server.test.js
@@ -3,29 +3,31 @@
 const { test } = require('tap')
 const server = require('../server.js')
 
-test('requests the "/" route', async t => {
-  const response = await server.inject({
-    method: 'GET',
-    url: '/'
-  })
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
-  t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
-})
+const scenarios = [
+  {
+    "uri": "/",
+    "statusCode": 200,
+    "body": /<[hH]1>\s*MCS Bike Bus/
+  },
+  {
+    "uri": "/manhattan-country-school",
+    "statusCode": 200,
+    "body": /<[hH]1>\s*MCS Bike Bus/
+  },
+  {
+    "uri": "/manhattan-country-school/",
+    "statusCode": 200,
+    "body": /<[hH]1>\s*MCS Bike Bus/
+  }
+]
 
-test('requests the "/manhattan-country-school/" route', async t => {
-  const response = await server.inject({
-    method: 'GET',
-    url: '/manhattan-country-school/'
+scenarios.forEach(scenario => {
+  test('requests the `' + scenario.uri + '` route', async t => {
+    const response = await server.inject({
+      method: 'GET',
+      url: scenario.uri
+    })
+    t.equal(response.statusCode, scenario.statusCode, 'returns a status code of 200')
+    t.match(response.body, scenario.body, 'defaults to Manhantan Country School')
   })
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
-  t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
-})
-
-test('requests the "/manhattan-country-school" route', async t => {
-  const response = await server.inject({
-    method: 'GET',
-    url: '/manhattan-country-school'
-  })
-  t.equal(response.statusCode, 200, 'returns a status code of 200')
-  t.match(response.body, /<[hH]1>\s*MCS Bike Bus/, 'defaults to Manhantan Country School')
 })
