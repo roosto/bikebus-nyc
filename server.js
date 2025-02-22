5282a3755da6145b4e7b919f225f2346de87b142
Joseph Frazier <1212jtraceur@gmail.com>
2025-02-22 18:47:06 -0500

    Default to `manhattan-country-school` when no bike route is specified in URL params

--- server.js
+++ server.js
@@ -48,19 +48,14 @@ const routes = require("./routes.json");
 server.get("/:routeKey", async function (request, reply) {
   let { routeKey } = request.params;
 
-  let routeKeys = [].concat(request.query.routeKey || []);
-  console.log({ routeKeys });
-
-  if (routeKey == "") {
-    routeKey = "manhattan-country-school";
-  }
+  let routeKeys = [].concat(request.query.routeKey || ["manhattan-country-school"]);
 
   let route;
 
-  if (!routes.hasOwnProperty(routeKey)) {
+  if (!routes.hasOwnProperty(routeKeys[0])) {
     return reply.code(404).type("text/plain").send("Route not found.");
   } else {
-    route = routes[routeKey];
+    route = routes[routeKeys[0]];
   }
 
   await storage.init();
