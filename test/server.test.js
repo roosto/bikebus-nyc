f21c0543295066702ea1d5539c9335a2288ecb6b
Dustø Masterson Goldman <dustin@goldmans.net>
2025-03-22 15:10:48 -0400

    Add Limited Testing (#8)
    * add test package, `tap`, in `devDependencies` group
    * alpha-order package dependencies
    * add Node TAP package
    * add basic GET / test
    * setup and array of inputs and expected values
    * add test command
    * ignore `.DS_store` files
    * allow incomplete coverage in tests: 
        TAP will by default exit non-zero when test coverage is < 100%, so this new invocation flag will override that.
    * remove unused packages: 
        these are obviated by use of TAP
    
    ---------
    
    Co-authored-by: Joseph Frazier <1212jtraceur@gmail.com>

---
+++ test/server.test.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { test } = require('tap')
+const server = require('../server.js')
+
+const scenarios = [
+  {
+    uri: "/",
+    expected: {
+      statusCode: 200,
+      body: /<[hH]1>\s*MCS Bike Bus/,
+    },
+  },
+  {
+    uri: "/manhattan-country-school",
+    expected: {
+      statusCode: 200,
+      body: /<[hH]1>\s*MCS Bike Bus/,
+    },
+  },
+]
+
+scenarios.forEach(scenario => {
+  test('requests the `' + scenario.uri + '` route',  async t => {
+    const response = await server.inject({
+      method: 'GET',
+      url: scenario.uri
+    })
+    t.equal(response.statusCode, scenario.expected.statusCode, 'returns a status code of 200')
+    t.match(response.body, scenario.expected.body, 'defaults to Manhattan Country School')
+  })
+})
