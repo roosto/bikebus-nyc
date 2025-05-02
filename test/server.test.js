55f8e0d998ed70755654670cef69d9ce8cb880fe
Dustin Goldman <dustin@goldmans.net>
2025-05-02 11:37:04 -0400

    add tests for getting route pages

--- test/server.test.js
+++ test/server.test.js
@@ -5,32 +5,33 @@ process.env.beacon_hash ||= 'HASH_FROM_TESTING'
 const { test } = require('tap')
 const server = require('../server.js')
 
-const tracker_scenarios = [
-  {
-    uri: "/",
-    expected: {
-      statusCode: 200,
-      body: /<[hH]1>\s*MCS Bike Bus/,
-    },
-  },
-  {
-    uri: "/manhattan-country-school",
-    expected: {
-      statusCode: 200,
-      body: /<[hH]1>\s*MCS Bike Bus/,
-    },
-  },
-]
-
-tracker_scenarios.forEach(scenario => {
-  test('requests the `' + scenario.uri + '` route', async t => {
-    const response = await server.inject({
-      method: 'GET',
-      url: scenario.uri
-    })
-    t.equal(response.statusCode, scenario.expected.statusCode, 'returns a status code of 200')
-    t.match(response.body, scenario.expected.body, 'defaults to Manhantan Country School')
+test('requests the `/manhattan-country-school` route', async t => {
+  const response = await server.inject({
+    method: 'GET',
+    url: '/manhattan-country-school'
   })
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+  t.match(response.body,  /<[hH]1>\s*MCS Bike Bus/, "<h1> tag contents match 'MCS Bike Bus")
+  t.match(response.body, /\{"manhattan-country-school":\{"title":"MCS Bike Bus: 1st Wednesdays Harlem to UWS"/, "Route JSON is embedded in page body")
+})
+
+test('requests the `/manhattan-country-school-too` route', async t => {
+  const response = await server.inject({
+    method: 'GET',
+    url: '/manhattan-country-school-too'
+  })
+  t.equal(response.statusCode, 200, 'returns a status code of 200')
+  t.match(response.body,  /<[hH]1>\s*\(TEST\) MCS Bike Bus/, "<h1> tag contenst match '(Test) MCS Bike Bus'")
+  t.match(response.body, /\{"manhattan-country-school-too":\{"title":"\(TEST\) MCS Bike Bus: 1st Wednesdays Harlem to UWS"/, "Route JSON is embedded in page body")
+})
+
+test('requests the `/I-AM-NOT_HERE` route', async t => {
+  const response = await server.inject({
+    method: 'GET',
+    url: '/I-AM-NOT_HERE'
+  })
+  t.equal(response.statusCode, 404, 'returns a status code of 404')
+  t.match(response.body,  /Route not found/, "page contains 'Route not found'")
 })
 
 test('requests the beacon page, w/o using a hash, at: `/beacon/manhattan-country-school/`', async t => {
