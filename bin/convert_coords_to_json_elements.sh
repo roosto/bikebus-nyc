3f0ba2bf944ff651997b553205aafe56ef15c535
Dustin Goldman <dustin@goldmans.net>
2026-04-07 19:29:39 -0400

    comment code more thoroughly

--- bin/convert_coords_to_json_elements.sh
+++ bin/convert_coords_to_json_elements.sh
@@ -14,7 +14,13 @@ then
     exit 1
 fi
 
+# we need to read from a file, instead of STDIN,
+# because we prompt user for a description & waypoint-ness
+# and if the `while` read loop is consuming STDIN, input for the prompts won't work
 filename="$1"
+
+# similarly, we need to write to a file instead of STDOUT,
+# because prompts from `read` will not go the terminal if STDOUT is redirected
 output_file="$2"
 if [[ ! -r "$filename" ]]
 then
@@ -27,6 +33,13 @@ then
     echo "$0: Error: Output file '$output_file' is not writable."
     exit 1
 fi
+
+## Convert coordinates to JSON elements for use as a stop in routes/{my_route}.json
+# $1: '[lat, long]' JSON array of coordinates
+# $2: description of the stop
+# $3: whether this stop is a waypoint only 'true' or 'false'
+#
+# a sigle JSON object with the above properties populated
 function to_element {
     local coords="$1"
     local desc="$2"
@@ -53,6 +66,8 @@ i=1
 for coords in "${coords_list[@]}"
 do
     read -p "desc for $i: " -r desc
+    # assume that waypoint is visible & labeled, b/c that is what's most useful
+    # when working on a new route or changes to existing route
     read -r -p "'$desc': \`wayPointOnly\`? [y/N]: " waypoint_only
 
     if [[ -n $waypoint_only && $waypoint_only =~ ^[yY] ]]
