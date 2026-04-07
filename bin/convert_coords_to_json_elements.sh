3c3dea84e440228ecd4b36860792eebb8afac144
Dustin Goldman <dustin@goldmans.net>
2026-04-07 13:51:41 -0400

    add help msg & arg validation
    * -h or --help for usage
    * must have 2 cli args
    * input file must be readable
    * output file must be writable

--- bin/convert_coords_to_json_elements.sh
+++ bin/convert_coords_to_json_elements.sh
@@ -2,8 +2,31 @@
 
 set -e -o pipefail
 
-filename="$1"
+if [[ "$*" =~ \b(-h|--help)\b ]]
+then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 0
+fi
 
+if [[ $# -ne 2 ]]
+then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+filename="$1"
+output_file="$2"
+if [[ ! -r "$filename" ]]
+then
+    echo "$0: Error: Input file '$filename' is not readable."
+    exit 1
+fi
+
+if ! touch "$output_file"
+then
+    echo "$0: Error: Output file '$output_file' is not writable."
+    exit 1
+fi
 function to_element {
     local coords="$1"
     local desc="$2"
