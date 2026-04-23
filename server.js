2fce855e4cdd58fd528e7c3ab3abc2f4fb980dfc
Dustin Goldman <dustin@goldmans.net>
2026-04-23 16:20:06 -0400

    DRY title, description, runInfo, days/times
    these values are all now defined in the route JSON files, and then dynamically rendered in the templates

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ handlebars.registerHelper("toJSON", function (object) {
   return new handlebars.SafeString(JSON.stringify(object));
 });
 
+handlebars.registerHelper('breaklines', function(text) {
+    text = handlebars.Utils.escapeExpression(text);
+    text = text.replace(/\n/gm, '<br>');
+    return new handlebars.SafeString(text);
+});
+
 server.register(require("@fastify/view"), {
   engine: {
     handlebars: handlebars,
